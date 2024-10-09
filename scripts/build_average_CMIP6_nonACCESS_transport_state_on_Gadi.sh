a67ad8d0d189ffe02a9bd1ebaba15658b145dba7
Benoit Pasquier <briochemc@gmail.com>
2024-10-09 17:06:19 +1100

    fix typo toddler programming

--- scripts/build_average_CMIP6_nonACCESS_transport_state_on_Gadi.sh
+++ scripts/build_average_CMIP6_nonACCESS_transport_state_on_Gadi.sh
@@ -3,8 +3,7 @@
 #PBS -P xv83
 #PBS -N CMIP6_nonACCESS_preprocessing
 #PBS -l ncpus=8
-#PBS -q hugemem
-#PBS -l mem=500GB
+#PBS -l mem=180GB
 #PBS -l jobfs=4GB
 #PBS -l walltime=48:00:00
 #PBS -l storage=gdata/xv83+gdata/dk92+gdata/oi10+gdata/hh5+gdata/xp65
@@ -40,10 +39,8 @@ year_start=1990
 num_years=10
 
 for model in CMCC-CM2-HR4 CMCC-CM2-SR5 CMCC-ESM2 FGOALS-f3-L FGOALS-g3 MPI-ESM-1-2-HAM MPI-ESM1-2-HR MPI-ESM1-2-LR NorCPM1 NorESM2-LM NorESM2-MM CESM2 CESM2-FV2 CESM2-WACCM-FV2 TaiESM1-TIMCOM; do
-    echo "Running transport-sg76v67mv65v>˘:"L>
-    ÷÷÷÷÷/,k(?≥÷≥;.tate script for $model model"
+    echo "Running transport-state script for $model model"
     python scripts/build_average_CMIP6_nonACCESS_transport_state_on_Gadi.py $model $experiment $ensemble $year_start $num_years \
     &> output/$model.$experiment.allensembles.$year_start.$num_years.$PBS_JOBID.out
 done
 
-/,k(?
