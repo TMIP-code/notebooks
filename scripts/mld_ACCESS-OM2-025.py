62be1474572450c32d0da5c06eb0cf5bf920506f
Benoit Pasquier <briochemc@gmail.com>
2025-11-24 13:39:32 +1100

    Try rechunking MLD files

--- scripts/mld_ACCESS-OM2-025.py
+++ scripts/mld_ACCESS-OM2-025.py
@@ -1,4 +1,3 @@
-
 # import sys to access script arguments (experiment, ensemble, first_year, last_year)
 import sys
 
@@ -10,6 +9,7 @@ subcatalog = sys.argv[1]
 
 # Ignore warnings
 from os import environ
+
 environ["PYTHONWARNINGS"] = "ignore"
 PROJECT = environ["PROJECT"]
 
@@ -41,13 +41,12 @@ import numpy as np
 # (to avoid too much boilerplate code)
 print("Defining functions")
 
+
 def select_data(cat, xarray_open_kwargs, **kwargs):
     selectedcat = cat.search(**kwargs)
     print("\nselectedcat: ", selectedcat)
-    xarray_combine_by_coords_kwargs=dict(
-        compat="override",
-        data_vars="minimal",
-        coords="minimal"
+    xarray_combine_by_coords_kwargs = dict(
+        compat="override", data_vars="minimal", coords="minimal"
     )
     datadask = selectedcat.to_dask(
         xarray_open_kwargs=xarray_open_kwargs,
@@ -56,15 +55,19 @@ def select_data(cat, xarray_open_kwargs, **kwargs):
     )
     return datadask
 
+
 def yearlymeans(ds):
     # Make a DataArray with the number of days in each month, size = len(time)
     month_length = ds.time.dt.days_in_month
     # Calculate the weights by grouping by 'time.year'
-    weights = month_length.groupby("time.year") / month_length.groupby("time.year").sum()
+    weights = (
+        month_length.groupby("time.year") / month_length.groupby("time.year").sum()
+    )
     # Test that the sum of the weights for each year is 1.0
     np.testing.assert_allclose(weights.groupby("time.year").sum().values, 1.0)
     # Calculate the weighted average
     return (ds * weights).groupby("time.year").sum(dim="time")
+
 
 # 3. Load catalog
 
@@ -75,13 +78,11 @@ cat = catalogs[subcatalog]
 print(cat)
 
 # Only keep the required data
-searched_cat = cat.search(variable = ["mld"])
+searched_cat = cat.search(variable=["mld"])
 print(searched_cat)
 
 # Create directory on scratch to save the data
-datadir = f'/scratch/{PROJECT}/TMIP/data'
-
-
+datadir = f"/scratch/{PROJECT}/TMIP/data"
 
 
 # 4. Load data, preprocess it, and save it to NetCDF
@@ -89,33 +90,33 @@ datadir = f'/scratch/{PROJECT}/TMIP/data'
 print("Starting client")
 
 # This `if` statement is required in scripts (not required in Jupyter)
-if __name__ == '__main__':
-    client = Client(n_workers=48, threads_per_worker=1) #, memory_limit='16GB') # Note: with 1thread/worker cannot plot thetao. Maybe I need to understand why?
+if __name__ == "__main__":
+    client = Client(
+        n_workers=48, threads_per_worker=1
+    )  # , memory_limit='16GB') # Note: with 1thread/worker cannot plot thetao. Maybe I need to understand why?
 
     # directory to save the data to (as NetCDF)
-    outputdir = f'{datadir}/{model}/{subcatalog}'
+    outputdir = f"{datadir}/{model}/{subcatalog}"
     print("Creating directory: ", outputdir)
     makedirs(outputdir, exist_ok=True)
 
     # mld
     try:
         print("Loading mld data")
-        mld_datadask = select_data(searched_cat,
-            dict(
-                chunks={'time':-1, 'yt_ocean':216, 'xt_ocean':240}
-            ),
-            variable = "mld",
-            frequency = "1mon",
+        mld_datadask = select_data(
+            searched_cat,
+            dict(chunks={"time": -1, "yt_ocean": 216, "xt_ocean": 240}),
+            variable="mld",
+            frequency="1mon",
         )
         print("\nmld_datadask: ", mld_datadask)
-        print("Saving mld to: ", f'{outputdir}/mld.nc')
-        mld.to_netcdf(f'{outputdir}/mld.nc', compute=True)
+        print("Saving mld to: ", f"{outputdir}/mld_rechunked.nc")
+        mld_datadask.mld.to_netcdf(
+            f"{outputdir}/mld_rechunked.nc",
+            encoding={"mld": {"chunksizes": [len(mld_datadask.mld.time), 216, 240]}},
+        )
     except Exception:
-        print(f'Error processing {model} mld')
+        print(f"Error processing {model} mld")
         print(traceback.format_exc())
 
     client.close()
-
-
-
-
